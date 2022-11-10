7c13315ffa493a774c8ac46923ae7447eb133863
Yuiichan <tranduybinh3010@gmail.com>
2022-11-10 17:28:22 +0700

    test

--- src/server.ts
+++ src/server.ts
@@ -3,14 +3,14 @@ import http from 'http';
 import mongoose from 'mongoose';
 import { config } from './config/config';
 
-mongoose
-  .connect(config.mongo.url, { retryWrites: true, w: 'majority' })
-  .then(() => {
-    console.log('Connected Successfully');
-  })
-  .catch((error) => {
-    console.log(error);
-  });
+// mongoose
+//   .connect(config.mongo.url, { retryWrites: true, w: 'majority' })
+//   .then(() => {
+//     console.log('Connected Successfully');
+//   })
+//   .catch((error) => {
+//     console.log(error);
+//   });
 
 const app = express();
 
